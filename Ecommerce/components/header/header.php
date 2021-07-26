70915680eae110045856f3fe620c438d6ed6b031
bennytran <taitran.developer@gmail.com>
2021-07-26 21:22:14 +0700

    finish login signup and footer header

---
+++ Ecommerce/components/header/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link rel='stylesheet' type='text/css' href='style.css' />
+    <title>Header</title>
+</head>
+<body>
+<nav class="navbar sticky-top navbar_icon_top navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Database Application</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+          <i class="fas fa-shopping-cart"></i>
+              <span class="badge badge-info">1</span>
+            </i>
+          </a>
+        </li>
+      </ul>
+    </div>
+</nav>
+</body>
