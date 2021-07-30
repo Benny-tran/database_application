5191feeda485ddde2ab122162c9ea99375fea604
Topovn <topovn0908@gmail.com>
2021-07-30 22:35:46 +0700

    in layout now

--- Ecommerce/components/pages/customer/auction.php
+++ Ecommerce/components/pages/customer/auction.php
@@ -6,159 +6,220 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Auction</title>
+
+    <!-- Bootstrap CSS CDN -->
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+        integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+    <!-- Our Custom CSS -->
+    <!-- <link rel="stylesheet" href="../layout/layout.css"> -->
     <link rel='stylesheet' type='text/css' href='style.css' />
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+        integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
+        crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
+        integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
+        crossorigin="anonymous"></script>
+
 </head>
 
 <body>
-    <?php include '../header/header.php' ?>
-    <!-- Sidebar template -->
+    <?php include '../header/header.php' ?> 
 
-    <?php include '../layout/layout.php' ?>
-
-    <section class="ftco-section">
-        <div class="container mt-5 mr-5">
-            <div class="row">
-                <div class="mt-3 col-md-12 mb-3">
-                    <form style="margin:left; width:85% ">
-                        <input id="search_bar" type="text" placeholder="Search by Name, Code...."
-                            style="width:100%;display:block">
-                        <!-- <i class="fa fa-search"></i> -->
-                    </form>
+    <div class="wrapper">
+        <!-- Sidebar Holder -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <div class="user-info">
+                    <h3>John Doe</h3>
+                    <h5">Customer</h5>
                 </div>
             </div>
-        </div>
+            <!-- Sidebar Menu -->
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-book"></i>
+                        <span>Marketplace</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-cubes"></i>
+                        <span>Manage Auction</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-gavel"></i>
+                        <span>Bid History</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-user"></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
 
-        <div class="container mr-5">
-            <div class="row justify-content-center">
-                <div class="mt-5 col-md-12 mb-5">
-                    <h2 class="heading-section">Product Auction List</h2>
-                </div>
-            </div>
+        <!-- Page Content Holder -->
+        <div id="content">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="table-wrap">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th scope="col">
-                                        <label class="control control--checkbox">
-                                            <input type="checkbox" class="js-check-all" />
-                                            <div class="control__indicator"></div>
-                                        </label>
-                                    </th>
-                                    <th>Name111</th>
-                                    <th>Code</th>
-                                    <th>Status</th>
-                                    <th>Original Price</th>
-                                    <th>Created At</th>
-                                    <th>Closed At</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">
-                                        <label class="control control--checkbox">
-                                            <input type="checkbox" />
-                                            <div class="control__indicator"></div>
-                                        </label>
-                                    </th>
-                                    <td>Iphone</td>
-                                    <td>IP</td>
-                                    <td><a href="#" class="btn btn-warning">Active</a></td>
-                                    <td>$100</td>
-                                    <td>Date 1</td>
-                                    <td>Date 2</td>
+                    <div class="container mt-5 mr-5">
+                        <div class="row">
+                            <div class="mt-3 col-md-12 mb-3">
+                                <form style="margin:left; width:85% ">
+                                    <input id="search_bar" type="text" placeholder="Search by Name, Code...."
+                                        style="width:100%;display:block">
+                                    <!-- <i class="fa fa-search"></i> -->
+                                </form>
+                            </div>
+                        </div>
+                    </div>
 
-                                </tr>
+                    <div class="container mr-5">
+                        <div class="row justify-content-center">
+                            <div class="mt-5 col-md-12 mb-5">
+                                <h2 class="heading-section">Product Auction List</h2>
+                            </div>
 
-                                <tr>
-                                    <th scope="row">
-                                        <label class="control control--checkbox">
-                                            <input type="checkbox" />
-                                            <div class="control__indicator"></div>
-                                        </label>
-                                    </th>
-                                    <td>Headphone</td>
-                                    <td>H</td>
-                                    <td><a href="#" class="btn btn-success">Completed</a></td>
-                                    <td>$100</td>
-                                    <td>Date 1</td>
-                                    <td>Date 2</td>
-                                </tr>
+                            <!-- Create Auction button -->
 
-                                <tr>
-                                    <th scope="row">
-                                        <label class="control control--checkbox">
-                                            <input type="checkbox" />
-                                            <div class="control__indicator"></div>
-                                        </label>
-                                    </th>
-                                    <td>Keyboard</td>
-                                    <td>K</td>
-                                    <td><a href="#" class="btn btn-warning">Active</a></td>
-                                    <td>$100</td>
-                                    <td>Date 1</td>
-                                    <td>Date 2</td>
-                                </tr>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="table-wrap">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">
+                                                    <label class="control control--checkbox">
+                                                        <input type="checkbox" class="js-check-all" />
+                                                        <div class="control__indicator"></div>
+                                                    </label>
+                                                </th>
+                                                <th>Name111</th>
+                                                <th>Code</th>
+                                                <th>Status</th>
+                                                <th>Original Price</th>
+                                                <th>Created At</th>
+                                                <th>Closed At</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">
+                                                    <label class="control control--checkbox">
+                                                        <input type="checkbox" />
+                                                        <div class="control__indicator"></div>
+                                                    </label>
+                                                </th>
+                                                <td>Iphone</td>
+                                                <td>IP</td>
+                                                <td><a href="#" class="btn btn-warning">Active</a></td>
+                                                <td>$100</td>
+                                                <td>Date 1</td>
+                                                <td>Date 2</td>
 
-                                <tr>
-                                    <th scope="row">
-                                        <label class="control control--checkbox">
-                                            <input type="checkbox" />
-                                            <div class="control__indicator"></div>
-                                        </label>
-                                    </th>
-                                    <td>Ipad</td>
-                                    <td>IPD</td>
-                                    <td><a href="#" class="btn btn-warning">Active</a></td>
-                                    <td>$100</td>
-                                    <td>Date 1</td>
-                                    <td>Date 2</td>
-                                </tr>
+                                            </tr>
 
-                                <tr>
-                                    <th scope="row">
-                                        <label class="control control--checkbox">
-                                            <input type="checkbox" />
-                                            <div class="control__indicator"></div>
-                                        </label>
-                                    </th>
-                                    <td>Iphone</td>
-                                    <td>IP2</td>
-                                    <td><a href="#" class="btn btn-success">Completed</a></td>
-                                    <td>$100</td>
-                                    <td>Date 1</td>
-                                    <td>Date 2</td>
-                                </tr>
+                                            <tr>
+                                                <th scope="row">
+                                                    <label class="control control--checkbox">
+                                                        <input type="checkbox" />
+                                                        <div class="control__indicator"></div>
+                                                    </label>
+                                                </th>
+                                                <td>Headphone</td>
+                                                <td>H</td>
+                                                <td><a href="#" class="btn btn-success">Completed</a></td>
+                                                <td>$100</td>
+                                                <td>Date 1</td>
+                                                <td>Date 2</td>
+                                            </tr>
+
+                                            <tr>
+                                                <th scope="row">
+                                                    <label class="control control--checkbox">
+                                                        <input type="checkbox" />
+                                                        <div class="control__indicator"></div>
+                                                    </label>
+                                                </th>
+                                                <td>Keyboard</td>
+                                                <td>K</td>
+                                                <td><a href="#" class="btn btn-warning">Active</a></td>
+                                                <td>$100</td>
+                                                <td>Date 1</td>
+                                                <td>Date 2</td>
+                                            </tr>
+
+                                            <tr>
+                                                <th scope="row">
+                                                    <label class="control control--checkbox">
+                                                        <input type="checkbox" />
+                                                        <div class="control__indicator"></div>
+                                                    </label>
+                                                </th>
+                                                <td>Ipad</td>
+                                                <td>IPD</td>
+                                                <td><a href="#" class="btn btn-warning">Active</a></td>
+                                                <td>$100</td>
+                                                <td>Date 1</td>
+                                                <td>Date 2</td>
+                                            </tr>
+
+                                            <tr>
+                                                <th scope="row">
+                                                    <label class="control control--checkbox">
+                                                        <input type="checkbox" />
+                                                        <div class="control__indicator"></div>
+                                                    </label>
+                                                </th>
+                                                <td>Iphone</td>
+                                                <td>IP2</td>
+                                                <td><a href="#" class="btn btn-success">Completed</a></td>
+                                                <td>$100</td>
+                                                <td>Date 1</td>
+                                                <td>Date 2</td>
+                                            </tr>
 
 
-                            </tbody>
-                        </table>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w3-bar text-center mt-5">
+                            <a href="#" class="w3-button">&laquo;</a>
+                            <a href="#" class="w3-button">1</a>
+                            <a href="#" class="w3-button">2</a>
+                            <a href="#" class="w3-button">3</a>
+                            <a href="#" class="w3-button">4</a>
+                            <a href="#" class="w3-button">5</a>
+                            <a href="#" class="w3-button">&raquo;</a>
+                        </div>
+
                     </div>
                 </div>
             </div>
-
-            <div class="w3-bar text-center mt-5">
-                <a href="#" class="w3-button">&laquo;</a>
-                <a href="#" class="w3-button">1</a>
-                <a href="#" class="w3-button">2</a>
-                <a href="#" class="w3-button">3</a>
-                <a href="#" class="w3-button">4</a>
-                <a href="#" class="w3-button">5</a>
-                <a href="#" class="w3-button">&raquo;</a>
-            </div>
-
         </div>
-    </section>
-
-
+    </div>
 
     <!-- page-wrapper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -169,7 +230,19 @@
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
 
-    <?php include '../footer/footer.php' ?>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+        crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
